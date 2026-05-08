24f79467087a992d211c8108295675912479bda1
Ramiro Harguiunteguy <ramirohar@gmail.com>
2026-05-08 15:36:58 -0300

    fix(crsc): correct paths in when fetching redpitaya repo

--- csrc/sources/curl_all.sh
+++ csrc/sources/curl_all.sh
@@ -1,8 +1,8 @@
-curl -O https://raw.githubusercontent.com/RedPitaya/RedPitaya/master/rp-api/api/include/redpitaya/rp.h
-curl -O https://raw.githubusercontent.com/RedPitaya/RedPitaya/master/rp-api/api/include/redpitaya/rp_acq.h
-curl -O https://raw.githubusercontent.com/RedPitaya/RedPitaya/master/rp-api/api/include/redpitaya/rp_acq_axi.h
-curl -O https://raw.githubusercontent.com/RedPitaya/RedPitaya/master/rp-api/api/include/redpitaya/rp_enums.h
-curl -O https://raw.githubusercontent.com/RedPitaya/RedPitaya/master/rp-api/api/include/redpitaya/rp_gen.h
-curl -O https://raw.githubusercontent.com/RedPitaya/RedPitaya/master/rp-api/api/include/redpitaya/version.h
+curl -O https://raw.githubusercontent.com/RedPitaya/RedPitaya/master/rp-api/api/include/rp.h
+curl -O https://raw.githubusercontent.com/RedPitaya/RedPitaya/master/rp-api/api/include/rp_acq.h
+curl -O https://raw.githubusercontent.com/RedPitaya/RedPitaya/master/rp-api/api/include/rp_acq_axi.h
+curl -O https://raw.githubusercontent.com/RedPitaya/RedPitaya/master/rp-api/api/include/rp_enums.h
+curl -O https://raw.githubusercontent.com/RedPitaya/RedPitaya/master/rp-api/api/include/rp_gen.h
+curl -O https://raw.githubusercontent.com/RedPitaya/RedPitaya/master/rp-api/api/include/common/version.h
 
 curl -L https://api.github.com/repos/RedPitaya/RedPitaya/branches/master | grep -o '"sha": "[^"]*' | grep -o '[^"]*$' | head -1 > sha.txt
